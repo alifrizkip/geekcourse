75a938373107bb392b264f15d20e92026a9afadf
Me <alifrizkipambudi@gmail.com>
2018-04-11 10:13:58 +0700

    create migration

---
+++ database/migrations/2018_04_10_150136_create_course_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourseUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('course_id');
+            $table->unsignedInteger('user_id');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_user');
+    }
+}

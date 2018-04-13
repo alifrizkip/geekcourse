7f592530f4391bc32f58590045cef32e38199a59
Me <alifrizkipambudi@gmail.com>
2018-04-13 10:42:02 +0700

    create the seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CategoriesTableSeeder::class);
+        // Users
+        $users = factory(\App\User::class)->times(10)->create();
+
+        // Admin membuat master category
+        $categories = factory(\App\Category::class)->times(10)->create();
+
+        $courses = [];
+
+        // Untuk setiap user punya 0 - 3 courses
+        foreach ($users as $user) {
+            $userCourses = factory(\App\Course:: class)
+                ->times(rand(0,3))
+                ->make();
+
+            // assosiakan course dengan category dan author
+            // setiap course punya beberapa video
+            foreach ($userCourses as $course) {
+                $course->author()->associate($user);
+                $course->category()->associate($categories->random()->getKey());
+                $course->save();
+            }
+
+            // setiap course punya beberapa video
+            foreach ($userCourses as $course) {
+                foreach ($range(1,5) as $index) {
+                    $video = factory(\App\Video::class)->make(['order' => $index]);
+                    $course->videos()->save($video);
+                }
+                $courses[] = $course;
+            }
+        }
+
+        // Wishlist
+        $courses = collect($courses);
+        foreach($users as $user) {
+            // @todo: tidak boleh menambahkan course punya sendiri ke wishlist
+            $user->wishlists()->sync([$courses->random()->getKey(), $courses->random()->getKey()]);
+        }
+        // Enrolled Course
     }
 }
